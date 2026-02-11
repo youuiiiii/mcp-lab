6a3e9d805c9b3e31a460ba9cbc2f23b61764a389
youuiiiii <richie.christian@student.umn.ac.id>
2026-02-11 11:40:22 +0700

    week2

---
+++ mcp-w2/app/index.tsx
@@ -0,0 +1,71 @@
+import { Text, View, Image, ScrollView, TextInput } from "react-native";
+
+export default function Index() {
+  return (
+    <ScrollView
+      style={{ flex: 1 }} 
+      
+      contentContainerStyle={{
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 20 
+      }}
+    >
+
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image source={require("../assets/images/favicon.png")}></Image>
+      <TextInput placeholder="This is a placeholder"></TextInput>
+
+      <Image source={require("../assets/image/Furina_Icon.webp")}></Image>
+      <Text>Ini Furina</Text>
+
+      <Image source={require("../assets/image/Hu_Tao_Icon.webp")}></Image>
+      <Text>Ini Hutao</Text>
+
+      <Image source={require("../assets/image/Kaedehara_Kazuha_Icon.webp")}></Image>
+      <Text>Ini Kazuha</Text>
+
+      <Image source={require("../assets/image/Lan_Yan_Icon.webp")}></Image>
+      <Text>Ini Lan Yan</Text>
+
+      <Image source={require("../assets/image/Yae_Miko_Icon.webp")}></Image>
+      <Text>Ini Yae Miko</Text>
+
+      <Image source={require("../assets/image/Zhongli_Icon.webp")}></Image>
+      <Text>Ini Zhongli</Text>
+
+      <Image source={require("../assets/image/Kaedehara_Kazuha_Icon.webp")}></Image>
+      <Text>Ini Kazuha</Text>
+
+      <Image source={require("../assets/image/Lan_Yan_Icon.webp")}></Image>
+      <Text>Ini Lan Yan</Text>
+
+      <Image source={require("../assets/image/Yae_Miko_Icon.webp")}></Image>
+      <Text>Ini Yae Miko</Text>
+
+      <Image source={require("../assets/image/Zhongli_Icon.webp")}></Image>
+      <Text>Ini Zhongli</Text><Image source={require("../assets/image/Kaedehara_Kazuha_Icon.webp")}></Image>
+      <Text>Ini Kazuha</Text>
+
+      <Image source={require("../assets/image/Lan_Yan_Icon.webp")}></Image>
+      <Text>Ini Lan Yan</Text>
+
+      <Image source={require("../assets/image/Yae_Miko_Icon.webp")}></Image>
+      <Text>Ini Yae Miko</Text>
+
+      <Image source={require("../assets/image/Zhongli_Icon.webp")}></Image>
+      <Text>Ini Zhongli</Text><Image source={require("../assets/image/Kaedehara_Kazuha_Icon.webp")}></Image>
+      <Text>Ini Kazuha</Text>
+
+      <Image source={require("../assets/image/Lan_Yan_Icon.webp")}></Image>
+      <Text>Ini Lan Yan</Text>
+
+      <Image source={require("../assets/image/Yae_Miko_Icon.webp")}></Image>
+      <Text>Ini Yae Miko</Text>
+
+      <Image source={require("../assets/image/Zhongli_Icon.webp")}></Image>
+      <Text>Ini Zhongli</Text>
+
+    </ScrollView>
+  );
+}
